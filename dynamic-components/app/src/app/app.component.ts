e495bd271b92c16b9f7e88fef10e1ee616858858
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2024-05-08 10:45:59 +0530

    destroy dynamic component

--- dynamic-components/app/src/app/app.component.ts
+++ dynamic-components/app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, ComponentRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { User } from './auth-form/auth-form.interface';
 import { AuthFormComponent } from "./auth-form/auth-form.component";
@@ -10,7 +10,7 @@ import { AuthFormComponent } from "./auth-form/auth-form.component";
   <div>
     <!-- <auth-form (submitted)="loginUser($event)"></auth-form> -->
     <div #entry><div>
-    
+    <button (click)="destroyComponent()">Destroy</button>
   </div>
   `,
   imports: [RouterOutlet, AuthFormComponent],
@@ -20,16 +20,22 @@ export class AppComponent {
 
   @ViewChild("entry", { read: ViewContainerRef, static: true }) entry!: ViewContainerRef;
 
+  componentRef!: ComponentRef<AuthFormComponent>;
+
   constructor(private viewContainer: ViewContainerRef) { }
 
   ngOnInit(): void {
-    var componentRef = this.entry.createComponent(AuthFormComponent);
-    componentRef.instance.title = "Create Account";
-    componentRef.instance.submitted.subscribe(this.loginUser);
+    this.componentRef = this.entry.createComponent(AuthFormComponent);
+    this.componentRef.instance.title = "Create Account";
+    this.componentRef.instance.submitted.subscribe(this.loginUser);
     //this.viewContainer.createComponent(AuthFormComponent); //loading at end
   }
 
   loginUser(user: User) {
     console.log('Login', user);
+  }
+
+  destroyComponent() {
+    this.componentRef?.destroy();
   }
 }
